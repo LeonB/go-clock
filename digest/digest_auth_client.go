58d0b49a6a41681506be022bc77ae46ee4773f6e
Leon Bogaert <leon@tim-online.nl>
2018-12-11 17:17:02 +0100

    Changed hardcoded timeout to 2 minutes

--- digest/digest_auth_client.go
+++ digest/digest_auth_client.go
@@ -156,7 +156,7 @@ func (dr *DigestRequest) executeRequest(authString string) (resp *http.Response,
 	req.Header.Add("Authorization", authString)
 
 	client := &http.Client{
-		Timeout: 30 * time.Second,
+		Timeout: 1 * time.Minute,
 	}
 
 	if !dr.CertVal {
